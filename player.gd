94bdb3de22f5b17036fa68ea011e99ede84b6001
JoshBump0 <bumpj@Sunypoly.edu>
2024-09-23 13:39:34 -0400

    Revert "Peer ID Connected to Tile Map"
    This reverts commit 127974195b67962c10cb84ddb2e0e2fff63ce8bb.

--- player.gd
+++ player.gd
@@ -11,34 +11,14 @@ const ACCELERATIOB = 300
 
 #Player ID which will be determined on creation
 #Based on number of current connected peers
-var Host_ID = 0
-var Client_ID = 0
+var Player_ID = 0
 
 
-#When Host instantitates the play it is given special player 1 priviliges
-#Also giving it an ID based on number of peers messed it up
-func _on_host_id_signal(ID) -> void:
-	Host_ID = ID
-	pass # Replace with function body.
-	
 #When the player is spawned into the 'tree' or scene it is giving
 #itself authority to itself multiplayer wise. Basically it gives you control
 #over your character
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
-	
-	#Host is having some issues when peer connects and keeping its orignial ID of 1
-	#So now if the peer did not connected by host its ID its Host ID is zero
-	#and it gets a client ID, again could not find a more elegant solution but it will work
-	#Could proabably get it down to one static ID on a function that does not get called multiple times
-	#Sorry, -Josh
-	if Host_ID == 0:
-		Client_ID = multiplayer.get_peers().size() + 1
-		print(Client_ID)
-	else:
-		#Nothing changes its id is still 1
-		Host_ID = 1
-	
 	
 #Every tic or game update 'delta' the move function is being called
 func _physics_process(delta):
@@ -72,7 +52,7 @@ func apply_movement(Accel):
 	#increase the players velocity to max speed
 	velocity += Accel
 	velocity = velocity.limit_length(MAX_SPEED)
-	
+	print(Player_ID)
 	
 
 #When the signal from the world turn order is actvated this function
@@ -80,8 +60,11 @@ func apply_movement(Accel):
 func _on_tile_map_order(x) -> void:
 	print("Player")
 	print(x)
-	if(Host_ID == x || Client_ID == x):
-		$CanvasLayer.show()
-	else:
-		$CanvasLayer.hide()
+	pass # Replace with function body.
+
+func _on_ready() -> void:
+		#Array is empty when host is present, plus 1 to give it plyaer ID 1
+	#everytime a player spawns in the scene this runs and it is given its ID
+	Player_ID = multiplayer.get_peers().size() + 1
+	print(Player_ID)
 	pass # Replace with function body.
