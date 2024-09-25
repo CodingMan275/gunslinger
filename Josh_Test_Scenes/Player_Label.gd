b181283a320a43a118b358fdd402a5c5b4127c87
JoshBump0 <bumpj@Sunypoly.edu>
2024-09-25 16:39:53 -0400

    Updating Turn Order
    Just made the visuals more clear, onto LAN next

---
+++ Josh_Test_Scenes/Player_Label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var Order = get_parent().get_parent().Player_ID
+	text = str("Player ", Order)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var Order = get_parent().get_parent().Player_ID
+	text = str("Player ", Order)
+	pass
