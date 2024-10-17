1075f91704ec2e38caa83006d658ed9872ac9c83
FireWarrior821 <owenrocks505@gmail.com>
2024-10-17 13:44:31 -0400

    sound FX

--- Main_Scene_Board/layer_0.gd
+++ Main_Scene_Board/layer_0.gd
@@ -1,6 +1,7 @@
 extends TileMapLayer
 
-
+@onready var Sound_Player = %Shuffle_Card
+@onready var Sound_Player2 = %Draw_Card
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fix_invalid_tiles()
@@ -10,3 +11,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_join_pressed():
+	Sound_Player.play()
+	pass # Replace with function body.
+
+
+func _on_draw_card_pressed() -> void:
+	Sound_Player2.play()
+	pass # Replace with function body.
+
+
+func _on_host_pressed() -> void:
+	Sound_Player.play()
+	pass # Replace with function body.
