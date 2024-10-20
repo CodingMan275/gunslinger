d031c65ed7f6e2fb7ceca15e9dfbf356972602dd
choakly <oakley1217@gmail.com>
2024-10-19 20:19:53 -0400

    townfolkgd

---
+++ CPU_and_Player/Townsfolk.gd
@@ -0,0 +1,13 @@
+extends "res://CPU_and_Player/Character.gd"
+class_name Townsfolk
+
+func _init():
+	pass
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
