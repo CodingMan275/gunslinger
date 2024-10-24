be85420544926eb07d64754c66edbf7098982741
demo-argenti <91711506+demo-argenti@users.noreply.github.com>
2024-10-24 12:08:34 -0400

    Tile detection implemented
    Created function so you can test to see what a given tile type is so we can create functions for ranged attacks and movement into buildings

--- Main_Scene_Board/layer_0.gd
+++ Main_Scene_Board/layer_0.gd
@@ -2,6 +2,14 @@ extends TileMapLayer
 
 @onready var Sound_Player = $"../CanvasLayer/Join/Shuffle cards"
 @onready var Sound_Player2 = $"../CanvasLayer/Draw Card/Draw cards"
+
+enum TerrainType{
+	DIRT_PATH = 1,
+	BOARDWALK = 2,
+	BUILDING = 3,
+	STABLE = 4
+}
+
 func _ready():
 	fix_invalid_tiles()
 
@@ -21,3 +29,16 @@ func _on_join_pressed():
 func _on_draw_card_pressed() -> void:
 	Sound_Player2.play()
 	pass # Replace with function body.
+	
+	
+func is_boardwalk_tile(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.BOARDWALK
+	
+func is_building_tile(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.BUILDING
+	
+func is_path_tile(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.DIRT_PATH
+	
+func is_stable_tile(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.STABLE
