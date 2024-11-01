fa23b69fd634ecde7f3e6c194cfebd6b0521bd26
Thegick76 <91561500+Thegick76@users.noreply.github.com>
2024-11-01 13:35:39 -0400

    take 2

---
+++ Main_Scene_Board/layer_0.gd
@@ -0,0 +1,92 @@
+extends TileMapLayer
+
+@onready var Sound_Player2 = $"../CanvasLayer/Draw Card/Draw cards"
+@onready var Sound_Player3 = $"../CanvasLayer/Attack/Attack"
+@onready var Sound_Player4 = $"../CanvasLayer/Brawl/Punch"
+
+enum TerrainType{
+	
+	# 7 BUILDING
+	STORE = 1,
+	CHURCH= 2,
+	DOCTOR = 3,
+	BANK = 4,
+	SHERRIF = 5,
+	SCHOOL = 6,
+	SALOON = 7,
+	# 3 Extras
+	JAIL = 8,
+	PATH = 9,
+	STABLE = 10,
+	# 6 BaordWalks
+	STOREB = 11,
+	BOOTHILL = 12,
+	DOCTORB = 13,
+	BANKB = 14,
+	SHERRIFB = 15,
+	YARD = 16
+}
+
+func _ready():
+	fix_invalid_tiles()
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Code to check the ENUM of a tile
+	"""
+	if Input.is_action_just_pressed("LeftClick"):
+		var location = get_node("../Layer0").local_to_map(Vector2(get_global_mouse_position()))
+		print(get_cell_tile_data(location).get_custom_data("TileType"))
+	"""
+		
+	pass
+
+func SameBuilding(Player : Vector2 , Enemy : Vector2) -> bool:
+	#If you both are on the same type of enum building
+	if get_cell_tile_data(Player).get_custom_data("TileType") == get_cell_tile_data(Enemy).get_custom_data("TileType"):
+		return true
+	# You are on the boardwalk connected to the building
+	elif get_cell_tile_data(Player).get_custom_data("TileType") - 10 == get_cell_tile_data(Enemy).get_custom_data("TileType"):
+		return true
+	else:
+		return false
+
+func Boardwalk(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return ENUM > 10
+
+func Building(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return ENUM < 8
+
+func WalledBuilding(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return (ENUM < 8 && ENUM %2 == 0)
+
+func Path(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.PATH
+	
+func Stable(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.STABLE
+
+func Jail(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.JAIL
+
+
+
+
+
+func _on_draw_card_pressed() -> void:
+	Sound_Player2.play()
+	pass # Replace with function body.
+
+func _on_attack_pressed() -> void:
+	Sound_Player3.play()
+	pass # Replace with function body.
+
+func _on_brawl_pressed() -> void:
+	Sound_Player4.play()
+	pass # Replace with function body.
