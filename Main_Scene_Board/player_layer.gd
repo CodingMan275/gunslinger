61d6d4024cbcfb6c6f05073f38ac678fbc59ca56
demo-argenti <91711506+demo-argenti@users.noreply.github.com>
2024-11-08 13:18:53 -0500

    gameplay now works on player_layer

--- Main_Scene_Board/player_layer.gd
+++ Main_Scene_Board/player_layer.gd
@@ -23,11 +23,50 @@ enum TerrainType{
 	YARD = 16
 }
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	fix_invalid_tiles()
+
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
+	# Code to check the ENUM of a tile
+	"""
+	if Input.is_action_just_pressed("LeftClick"):
+		var location = get_node("../Layer0").local_to_map(Vector2(get_global_mouse_position()))
+		print(get_cell_tile_data(location).get_custom_data("TileType"))
+	"""
+		
 	pass
+
+func SameBuilding(Player : Vector2 , Enemy : Vector2) -> bool:
+	#If you both are on the same type of enum building
+	if get_cell_tile_data(Player).get_custom_data("TileType") == get_cell_tile_data(Enemy).get_custom_data("TileType"):
+		return true
+	# You are on the boardwalk connected to the building
+	elif get_cell_tile_data(Player).get_custom_data("TileType") - 10 == get_cell_tile_data(Enemy).get_custom_data("TileType"):
+		return true
+	else:
+		return false
+
+func Boardwalk(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return ENUM > 10
+
+func Building(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return ENUM < 8
+
+func WalledBuilding(location : Vector2) -> bool:
+	var ENUM = get_cell_tile_data(location).get_custom_data("TileType")
+	return (ENUM < 8 && ENUM %2 == 0)
+
+func Path(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.PATH
+	
+func Stable(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.STABLE
+
+func Jail(location : Vector2) -> bool:
+	return get_cell_tile_data(location).get_custom_data("TileType") == TerrainType.JAIL
