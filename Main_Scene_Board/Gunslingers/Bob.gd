5a254c430091db884a70ee381a72fa5dc10761c6
choakly <oakley1217@gmail.com>
2024-11-08 17:52:15 -0500

    attacking cnavas
    All the attacking canvas for the townsfolks, can be used to add the special ability buttons too, and should be easy way to prevent some characters from brawling
    
    See JOSH I use this

--- Main_Scene_Board/Gunslingers/Bob.gd
+++ Main_Scene_Board/Gunslingers/Bob.gd
@@ -1,6 +1,16 @@
 extends Node
 
 
+var KnifeProf = 1
+var PistolProf = 0
+var RifleProf = -1
+var ShotgunProf = 0
+var TwinPistolProf = 0
+var BrawlProf = -1
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	'''
