3803e4c58f445547f6b5d4d7cbd177cefbd2685a
choakly <oakley1217@gmail.com>
2024-11-14 14:48:14 -0500

    Revert "Fixed UI changes."
    This reverts commit 8359f4f403634e822b761f0a3358529def8a257a.

--- pause_menu/pause_menu_button_singleplayer.gd
+++ pause_menu/pause_menu_button_singleplayer.gd
@@ -3,12 +3,11 @@ extends Button
 @onready var pause_menu_singleplayer = %PauseMenuSingleplayer
 @onready var pause_menu_button_singleplayer = %PauseMenuButtonSinglePlayer
 
-
-
 func _ready():
 	if GlobalScript.SinglePlay == true:
 		pause_menu_button_singleplayer.show()
 		print("Singleplayer Button visible")
 
 func _on_pressed():
+	#get_tree().paused = true
 	pause_menu_singleplayer.show()
