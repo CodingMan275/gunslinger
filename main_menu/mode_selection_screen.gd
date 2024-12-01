5cb9b49700a78d7f38d545d1c5ccb5b2ded3d352
Riverad4 <93441413+Riverad4@users.noreply.github.com>
2024-12-01 14:09:25 -0500

    Music Bug Fixed
    Music for the main menu, options screen, and mode selection screen have a better music flow. When going to the game scenes (single-player and/or multiplayer) the music stops. And when returning to the main menu, the music plays again.

--- main_menu/mode_selection_screen.gd
+++ main_menu/mode_selection_screen.gd
@@ -5,10 +5,11 @@ extends Control
 func _on_local_button_pressed():
 	GlobalScript.SinglePlay = true
 	get_tree().change_scene_to_file("res://Main_Scene_Board/node_2d.tscn")
-	
+	AudioPlayer.stop()
 
 
 func _on_online_button_pressed():
+	AudioPlayer.stop()
 	GlobalScript.SinglePlay = false
 	get_tree().change_scene_to_file("res://Josh_Test_Scenes/world.tscn")
 
