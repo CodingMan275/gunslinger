744b22350fa395b712c0f716bbfa25049ffb94af
Riverad4 <93441413+Riverad4@users.noreply.github.com>
2024-12-04 23:12:08 -0500

    Save System (WIP)
    Adding more things to the save system, added new continue button art, added background art for CPUs 1 & 2.

--- main_menu/mode_selection_screen.gd
+++ main_menu/mode_selection_screen.gd
@@ -1,12 +1,14 @@
 extends Control
 
+@onready var single_player_warning_message = %"Single-Player_Warning_Message"
+@onready var save_overwriting_message = %Save_Overwriting_Message
 
 
 func _on_local_button_pressed():
 	GlobalScript.SinglePlay = true
 	get_tree().change_scene_to_file("res://Main_Scene_Board/node_2d.tscn")
 	AudioPlayer.stop()
-
+	#single_player_warning_message.show()
 
 func _on_online_button_pressed():
 	AudioPlayer.stop()
@@ -15,4 +17,17 @@ func _on_online_button_pressed():
 
 
 func _on_continue_button_pressed():
-	pass # Replace with function body.
+	#Just for testing the buttons in the single_player_warning_message
+	single_player_warning_message.show()
+
+
+func _on_yes_button_pressed():
+	save_overwriting_message.show()
+	await get_tree().create_timer(2).timeout
+	save_overwriting_message.hide()
+	GlobalScript.SinglePlay = true
+	get_tree().change_scene_to_file("res://Main_Scene_Board/node_2d.tscn")
+
+
+func _on_no_button_pressed():
+	single_player_warning_message.hide()
--- pause_menu/pause_menu_singleplayer.gd
+++ pause_menu/pause_menu_singleplayer.gd
@@ -6,7 +6,8 @@ extends Control
 @onready var pause_menu_options_screen = %PauseMenuOptionsScreen
 @onready var close_options_menu_button = %CloseOptionsMenuButton
 @onready var RuleController = $"../../../Rules_Controller"
-
+@onready var game_saved_message = %Game_Saved_Message
+@onready var game_loading_message = %Game_Loading_Message
 
 
 
@@ -29,12 +30,17 @@ func _on_quit__button_pressed():
 	RuleController.Winner(-1 , false)
 
 func _on_load_button_pressed():
-	pass
-
+	game_loading_message.show()
+	await get_tree().create_timer(2).timeout
+	game_loading_message.hide()
+	margin_container.show()
+	margin_container_2.hide()
+	pause_menu_singleplayer.hide()
 
 func _on_save_button_pressed():
-	pass
-
+	game_saved_message.show()
+	await get_tree().create_timer(2).timeout
+	game_saved_message.hide()
 
 func _on_cancel_pressed():
 	margin_container.show()
